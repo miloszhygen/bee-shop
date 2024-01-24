d3ccd7c6f38ebcf7841bb0cf2d89e93648834b5f
miloszhygen <miloszhygen@gmail.com>
2024-01-24 11:45:37 +0100

    Show an image of a product on the page component
    - updated next.config to allow images from stripe

--- app/page.js
+++ app/page.js
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Image
+        src="https://files.stripe.com/links/MDB8YWNjdF8xT0dwc1RJNEFGT1hqelRDfGZsX3Rlc3RfQnZIcHEzNVV3bDlta210RDBLOTFiRXli00JA35wqUI"
+        alt="Vercel Logo"
+        width={400}
+        height={300}
+        priority
+      />
+    </main>
+  );
 }
