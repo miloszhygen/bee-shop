51573d4fe7fccccec602e78f7a0c678cb171432e
miloszhygen <miloszhygen@gmail.com>
2024-01-24 22:45:52 +0100

    Updated the page.js file to handle the new components
    - added the new shop container component
    - sent the fetchAllProducts action as a prop to the shop container

--- app/page.js
+++ app/page.js
@@ -1,16 +1,12 @@
-import Image from "next/image";
-import { PageLayout } from "@/components/layout/PageLayout";
+import { fetchAllProducts } from "@/actions/fetchAllProducts";
+
+import PageLayout from "@/components/layout/PageLayout";
+import ShopContainer from "@/components/pages/shopContainer";
 
 export default function Home() {
   return (
     <PageLayout>
-      <Image
-        src="https://files.stripe.com/links/MDB8YWNjdF8xT0dwc1RJNEFGT1hqelRDfGZsX3Rlc3RfQnZIcHEzNVV3bDlta210RDBLOTFiRXli00JA35wqUI"
-        alt="Vercel Logo"
-        width={400}
-        height={300}
-        priority
-      />
+      <ShopContainer fetchAllProducts={fetchAllProducts} />
     </PageLayout>
   );
 }
