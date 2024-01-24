3494f79131c17ac16b9c80a5999bdb8c564d549d
miloszhygen <miloszhygen@gmail.com>
2024-01-24 22:48:49 +0100

    Updated the fetchAllProducts to map for product price from the price response

--- actions/fetchAllProducts.js
+++ actions/fetchAllProducts.js
@@ -1,5 +1,20 @@
+"use server";
+
 import mockProductsList from "@/mockData/products";
+import mockPricesList from "@/mockData/prices";
 
 export async function fetchAllProducts() {
-  return mockProductsList;
+  const productsResponse = mockProductsList.data;
+
+  const products = productsResponse.map((product) => {
+    const price = mockPricesList.data.find(
+      (price) => price?.id === product?.default_price
+    );
+    return {
+      ...product,
+      price: price?.unit_amount,
+    };
+  });
+
+  return products;
 }
