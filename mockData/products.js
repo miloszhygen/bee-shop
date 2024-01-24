f6a3d7bdb1637601a192ceee0cf26cefdcb6278d
miloszhygen <miloszhygen@gmail.com>
2024-01-24 22:49:57 +0100

    Added a secont product in the mockdata

--- mockData/products.js
+++ mockData/products.js
@@ -1,4 +1,4 @@
-export const mockProductsList = {
+const mockProductsList = {
   object: "list",
   data: [
     {
@@ -16,6 +16,30 @@ export const mockProductsList = {
       livemode: false,
       metadata: {},
       name: "Alpine Blossom Honey",
+      package_dimensions: null,
+      shippable: null,
+      statement_descriptor: null,
+      tax_code: "txcd_20030000",
+      type: "service",
+      unit_label: null,
+      updated: 1706091582,
+      url: null,
+    },
+    {
+      id: "prod_PQvPpUEDyLZgwe",
+      object: "product",
+      active: true,
+      attributes: [],
+      created: 1706091581,
+      default_price: "price_1Oc3YXI4AFOXjzTClsRL4cRn",
+      description: "Harvested from wildflowers in the Alpine meadows.",
+      features: [],
+      images: [
+        "https://files.stripe.com/links/MDB8YWNjdF8xT0dwc1RJNEFGT1hqelRDfGZsX3Rlc3RfQnZIcHEzNVV3bDlta210RDBLOTFiRXli00JA35wqUI",
+      ],
+      livemode: false,
+      metadata: {},
+      name: "Alpine Blossom Honey 2",
       package_dimensions: null,
       shippable: null,
       statement_descriptor: null,
