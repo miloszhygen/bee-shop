8f2896dba8efeb4dba0055dad595fe43997c171b
miloszhygen <miloszhygen@gmail.com>
2024-01-25 10:51:52 +0100

    Updated the basket component to handle selected products
    - it shows product and amount
    - it calculates the price * amount and shows it
    - it calculates the total
    - added “go to payment” button

--- components/parts/Basket.jsx
+++ components/parts/Basket.jsx
@@ -4,14 +4,30 @@ import BasketContext from "@/context/basketContext";
 export default function Basket() {
   const { basketProducts, setBasketProducts } = useContext(BasketContext);
 
+  const removeProduct = (name) => {
+    setBasketProducts(
+      basketProducts.filter((product) => product.name !== name)
+    );
+  };
+
   return (
     <div>
       <h3>BASKET</h3>
       <ul>
         {basketProducts.map((product) => (
-          <li key={product?.name}>{product?.name} REMOVE</li>
+          <li key={product?.id}>
+            {product?.count} x {product?.name} ({product?.price / 100} {"NOK"})
+            | {(product?.price * product?.count) / 100} {"NOK"}
+            <div onClick={() => removeProduct(product.name)}>remove</div>
+          </li>
         ))}
       </ul>
+      TOTAL:{" "}
+      {basketProducts.reduce((acc, curr) => acc + curr.price * curr.count, 0) /
+        100}{" "}
+      <div>
+        <button>Go to payment</button>
+      </div>
     </div>
   );
 }
