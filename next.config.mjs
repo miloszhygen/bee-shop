78d5b1534fb6d60d19a32ec2b872dfaac49939a6
miloszhygen <miloszhygen@gmail.com>
2024-01-28 20:43:22 +0100

    images.domains depreached in next, use remotePatterns instead

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["files.stripe.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "files.stripe.com",
+      },
+    ],
   },
   experimental: {
     missingSuspenseWithCSRBailout: false,
