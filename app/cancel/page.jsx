5bebf46c45fb23b5b96653bdffc9ded4bbbbc866
miloszhygen <miloszhygen@gmail.com>
2024-02-01 22:21:39 +0100

    Added a cancel page when payment is canceled

---
+++ app/cancel/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+const CancelPage = () => {
+  return (
+    <div className="thank-you-page">
+      IMAGE SAD BEE
+      <h1>Payment canceled!</h1>
+    </div>
+  );
+};
+
+export default CancelPage;
