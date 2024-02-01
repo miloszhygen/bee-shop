4f026cf12b22a1f665c22f2d8a0043a63b90127f
miloszhygen <miloszhygen@gmail.com>
2024-02-01 10:56:05 +0100

    Added a create checkout api endpoint to create a payment session
    -the session is used by the client to redirect the user to a stripe payment page

---
+++ app/api/checkout/route.js
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { nanoid } from "nanoid";
+
+import Stripe from "stripe";
+
+export async function POST(request) {
+  // Session stamp is used to identify the session on the thank you page
+  // and for the transation in set in database IF user is not logged in
+  const sessionStamp = nanoid();
+
+  const data = await request.json();
+  const { lineItems } = data;
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+  // Doc: https://stripe.com/docs/api/checkout/sessions/create
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [...lineItems],
+    metadata: {
+      user_id: "",
+      sessionStamp,
+    },
+    mode: "payment",
+    success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/thank-you?id=${sessionStamp}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/cancel`,
+  });
+
+  return NextResponse.json({ id: session.id });
+}
