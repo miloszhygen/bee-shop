4ddd30b2f5f52cd57c1214ab1b0f661897099ac4
miloszhygen <miloszhygen@gmail.com>
2024-02-08 11:06:17 +0100

    Fetch all products from stripe if test variable is set to false

--- actions/fetchAllProductsAction.js
+++ actions/fetchAllProductsAction.js
@@ -2,12 +2,38 @@
 
 import mockProductsList from "@/mockData/products";
 import mockPricesList from "@/mockData/prices";
+import Stripe from "stripe";
+
+import { TEST } from "@/config";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function fetchAllProductsAction() {
-  const productsResponse = mockProductsList.data;
+  /*
+    created.gte: This will return all products that were created on or after the specified date (inclusive). This means if a product was created exactly at the specified date and time, it will be included in the results.
+
+  */
+  const stripeProductsResponse = TEST
+    ? mockProductsList
+    : await stripe.products.list({
+        active: true,
+        created: { gte: 1706091581 },
+      });
+
+  const stripePricesResponse = TEST
+    ? mockPricesList
+    : await stripe.prices.list({
+        active: true,
+        created: { gte: 1706091581 },
+      });
+
+  // TODO: If one fetch fails send error message back "could not fetch products, try to refresh the page."
+
+  const { data: productsResponse } = stripeProductsResponse;
+  const { data: pricesResponse } = stripePricesResponse;
 
   const products = productsResponse.map((product) => {
-    const price = mockPricesList.data.find(
+    const price = pricesResponse.find(
       (price) => price?.id === product?.default_price
     );
     return {
